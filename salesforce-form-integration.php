0f1d32a06acc5be3ef34215896697fb3c90a1486
Michael Dance <michael.dance@agencychief.com>
2016-06-28 09:34:42 -0400

    Added plugin files and header info

---
+++ salesforce-form-integration.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Salesforce Form Integration
+Description: Integrate Salesforce with WordPress forms. Compatible with Formidable Forms, Gravity Forms, and Contact Form 7.
+Version:     1.0
+Author:      CHIEF
+Author URI:  http://www.agencychief.com
+Text Domain: salesforce-form-integration
+*/
